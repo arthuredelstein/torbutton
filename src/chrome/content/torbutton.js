40f35d5b280cf694d2a6cb3ade734511eed5593e
Arthur Edelstein <arthuredelstein@gmail.com>
2017-10-26 22:04:43 -0700

    Bug 23997: Add link to Tor Browser manual for de, nl, tr, vi

--- src/chrome/content/torbutton.js
+++ src/chrome/content/torbutton.js
@@ -2357,7 +2357,7 @@ function torbutton_update_noscript_button()
 
 // Returns true if we should show the tor browser manual.
 function torbutton_show_torbrowser_manual() {
-  let availableLocales = ["en", "es", "fr", "pt", "zh"];
+  let availableLocales = ["de", "en", "es", "fr", "nl", "pt", "tr", "vi", "zh"];
   let shortLocale = torbutton_get_general_useragent_locale().substring(0, 2);
   return availableLocales.indexOf(shortLocale) >= 0;
 }
