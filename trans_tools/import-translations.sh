bdb6158c6cb74d40663894b3d519ff405bd0df31
Arthur Edelstein <arthuredelstein@gmail.com>
2018-08-13 14:35:33 -0700

    Bug 27129: Add locales ca, ga, id, is, nb

--- trans_tools/import-translations.sh
+++ trans_tools/import-translations.sh
@@ -2,7 +2,7 @@
 
 # This var comes from the TBB locale list.
 # XXX: Find some way to keep this, tor-launcher, and Tor Browser in sync
-BUNDLE_LOCALES="ar bn-BD da de es fa fr he it ja ko nl pl pt-BR ru sv tr vi zh-CN zh-TW"
+BUNDLE_LOCALES="ar bn-BD ca da de es fa fr ga he id is it ja ko nb nl pl pt-BR ru sv tr vi zh-CN zh-TW"
 
 # XXX: Basque (eu) by request in #10687.
 # This is not used for official builds, but should remain 
