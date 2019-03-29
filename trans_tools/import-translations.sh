ad0f608cba44dc8be382b380b844cfd66dedecba
Georg Koppen <gk@torproject.org>
2019-03-29 09:39:56 +0000

    Bug 29943: Use locales in AB-CD scheme to match Mozilla

--- trans_tools/import-translations.sh
+++ trans_tools/import-translations.sh
@@ -2,13 +2,12 @@
 
 # This var comes from the TBB locale list.
 # XXX: Find some way to keep this, tor-launcher, and Tor Browser in sync
-BUNDLE_LOCALES="ar ca cs da de el es fa fr ga he hu id is it ja ka ko nb nl pl pt-BR ru sv tr vi zh-CN zh-TW"
+BUNDLE_LOCALES="ar ca cs da de el es-ES fa fr ga-IE he hu id is it ja ka ko nb-NO nl pl pt-BR ru sv-SE tr vi zh-CN zh-TW"
 
 # XXX: Basque (eu) by request in #10687.
-# This is not used for official builds, but should remain 
-# so Basque XPIs can be build independently. We can do
-# this for other languages too, if anyone requests this
-# and translations are available.
+# This is not used for official builds, but should remain so Basque XPIs can be
+# built independently. We can do that for other languages too, if anyone
+# requests it and translations are available.
 # XXX: Adding bn-BD as well as we don't ship that locale ourselves due to bug
 # 26498. Others might want to fix, build, and use it, though.
 BUNDLE_LOCALES="$BUNDLE_LOCALES eu bn-BD"
@@ -76,10 +75,9 @@ for KEYVAL in "${FILEMAP[@]}"; do
   git merge -q origin/"$BRANCH"
   for i in $BUNDLE_LOCALES;
   do
-    UL="`echo $i|tr - _`"
     mkdir -p ../$LOCALE_DIR/$i/
 # Some file names are lowercase in Transifex.
-    if [ -f $UL/"$DEST_FILE" ]; then
+    if [ -f $i/"$DEST_FILE" ]; then
       SRCFILE="$DEST_FILE"
     else
       SRCFILE="`echo $DEST_FILE | tr '[:upper:]' '[:lower:]'`"
@@ -87,6 +85,6 @@ for KEYVAL in "${FILEMAP[@]}"; do
 # Use sed to work around a Transifex "double entity" issue.
     sed -e 's/\&amp;brandShortName;/\&brandShortName;/g'			\
         -e 's/\&amp;vendorShortName;/\&vendorShortName;/g'			\
-        $UL/"$SRCFILE" > ../$LOCALE_DIR/$i/"$DEST_FILE"
+        $i/"$SRCFILE" > ../$LOCALE_DIR/$i/"$DEST_FILE"
   done
 done
