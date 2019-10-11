73a43f2f4d846b2870757d7aa18a1b33643ba2b5
Georg Koppen <gk@torproject.org>
2019-10-11 10:23:22 +0000

    Bug 27268: Preference clean up

--- chrome/content/torbutton.js
+++ chrome/content/torbutton.js
@@ -1464,9 +1464,7 @@ function torbutton_update_fingerprinting_prefs() {
     var letterboxing = m_tb_prefs.getBoolPref("privacy.resistFingerprinting.letterboxing", false);
 
     m_tb_prefs.setBoolPref("webgl.disable-extensions", mode);
-    m_tb_prefs.setBoolPref("dom.network.enabled", !mode);
     m_tb_prefs.setBoolPref("dom.enable_performance", !mode);
-    m_tb_prefs.setBoolPref("plugin.expose_full_path", !mode);
     m_tb_prefs.setBoolPref("browser.zoom.siteSpecific", !mode);
     m_tb_prefs.setBoolPref("extensions.torbutton.resize_new_windows", mode && !letterboxing);
 
